e34a607b651080c8140283cf282dd2b6a1284149
student1 <student1@planetrobotics.com>
2023-05-01 20:06:22 -0400

    Number Guesser

--- App-NumberGuesser.py
+++ App-NumberGuesser.py
@@ -1,5 +1,11 @@
 import customtkinter as ctk
 
+def enter_clicked():
+    userName = entry.get()
+    addString = address.get()
+    print(userName)
+    print(addString)
+
 app = ctk.CTk()
 app.geometry("750x500")
 app.title("Number Guesser")
@@ -10,5 +16,8 @@ labelGuessed.grid(row=0, column=0, padx=5, pady=5)
 Geussed = ctk.CTkEntry(app, placeholder_text="Your number :   ",
                        font=ctk.CTkFont(size=13))
 Geussed.grid(row=0, column=1, padx=5, pady=5)
+
+btn = ctk.CTkButton(app, text="Enter", command = enter_clicked)
+btn.grid(row=1, column=1, padx=5, pady=5)
 
 app.mainloop()
